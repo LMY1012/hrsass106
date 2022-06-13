b31ecb41193916fec5b44f237c4f78378a4e798a
itheima <itheima@itcast.cn>
2022-06-13 13:32:48 +0800

    人力资源项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 浏览器中的标题
+  title: '人事管理',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 头部固定
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 侧边栏logo是否显示
+  sidebarLogo: false
+}
